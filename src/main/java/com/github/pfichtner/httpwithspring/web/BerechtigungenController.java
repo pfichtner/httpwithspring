a602a5a4dc498c077555491703e06ab763f4c8f3
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-06-28 14:28:23 +0200

    take use of RequestMapping

--- src/main/java/com/github/pfichtner/httpwithspring/web/BerechtigungenController.java
+++ src/main/java/com/github/pfichtner/httpwithspring/web/BerechtigungenController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pfichtner.httpwithspring.domain.BerechtigungenService;
@@ -20,21 +21,22 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/berechtigungen")
 public class BerechtigungenController {
 
 	private final BerechtigungenService service;
 
-	@GetMapping("/berechtigungen/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<BerechtigungDTO> getBerechtigung(@PathVariable UUID id) {
 		return ResponseEntity.of(service.load(new BerechtigungsId(id)).map(BerechtigungDTO::fromDomain));
 	}
 
-	@PutMapping("/berechtigungen/{id}")
+	@PutMapping("/{id}")
 	public void putBerechtigung(@PathVariable UUID id, @RequestBody BerechtigungDTO berechtigung) {
 		service.save(berechtigung.toDomain(id));
 	}
 
-	@DeleteMapping("/berechtigungen/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Object> deleteBerechtigung(@PathVariable UUID id) {
 		boolean deleted = service.delete(new BerechtigungsId(id));
 		return (deleted ? noContent() : notFound()).build();
