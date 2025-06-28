d79fd9296b429767e3481971d601d81e28e346c5
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-06-28 14:32:31 +0200

    controller does not have to been public

--- src/main/java/com/github/pfichtner/httpwithspring/web/BerechtigungenController.java
+++ src/main/java/com/github/pfichtner/httpwithspring/web/BerechtigungenController.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/berechtigungen")
-public class BerechtigungenController {
+class BerechtigungenController {
 
 	private final BerechtigungenService service;
 
