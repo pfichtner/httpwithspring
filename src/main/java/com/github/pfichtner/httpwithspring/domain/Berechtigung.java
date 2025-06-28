644561e95d5ff110bc4d5ad44169f6ac4c2fe56c
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-06-28 20:54:09 +0200

    removed some lombok annotations

--- src/main/java/com/github/pfichtner/httpwithspring/domain/Berechtigung.java
+++ src/main/java/com/github/pfichtner/httpwithspring/domain/Berechtigung.java
@@ -6,18 +6,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Entity
 @Data
 @FieldDefaults(level = PRIVATE)
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder(toBuilder = true)
 public class Berechtigung {
 
 	@EmbeddedId
