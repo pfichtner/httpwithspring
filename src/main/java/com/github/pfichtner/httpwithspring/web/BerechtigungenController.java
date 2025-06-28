d38deea509a8a362ece98e1a46eb0b8af735ae09
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-06-28 14:28:23 +0200

    take use of RequestMapping

--- src/main/java/com/github/pfichtner/httpwithspring/web/BerechtigungenController.java
+++ src/main/java/com/github/pfichtner/httpwithspring/web/BerechtigungenController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pfichtner.httpwithspring.data.BerechtigungenJpaRepository;
@@ -20,22 +21,23 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/berechtigungen")
 public class BerechtigungenController {
 
 	private final BerechtigungenJpaRepository repo;
 
-	@GetMapping("/berechtigungen/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Berechtigung> getBerechtigung(@PathVariable UUID id) {
 		return ResponseEntity.of(repo.findById(id));
 	}
 
-	@PutMapping("/berechtigungen/{id}")
+	@PutMapping("/{id}")
 	public void putBerechtigung(@PathVariable UUID id, @RequestBody Berechtigung berechtigung) {
 		berechtigung.setId(id);
 		repo.save(berechtigung);
 	}
 
-	@DeleteMapping("/berechtigungen/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Object> deleteBerechtigung(@PathVariable UUID id) {
 		boolean wasPresent = repo.findById(id).isPresent();
 		repo.deleteById(id);
